e195f4440e409d622cd065cbb27df71e0faf86fb
Lubosz <lubosz@gmail.com>
2010-11-29 21:53:28 +0100

    removed some win defines

--- include/TwPrecomp.h
+++ include/TwPrecomp.h
@@ -16,31 +16,17 @@
 
 #define GL_GLEXT_PROTOTYPES 1
 
-#if defined _MSC_VER
-#   pragma warning(disable: 4514)   // unreferenced inline function has been removed
-#   pragma warning(disable: 4710)   // function not inlined
-#   pragma warning(disable: 4786)   // template name truncated
-#   pragma warning(disable: 4530)   // exceptions not handled
-#   define _CRT_SECURE_NO_DEPRECATE // visual 8 secure crt warning
-#endif
 
 #include <stdio.h>
 #include <assert.h>
 #include <math.h>
 #include <float.h>
-
-#if defined(_MSC_VER) && _MSC_VER<=1200
-#   pragma warning(push, 3)
-#endif
 #include <string>
 #include <sstream>
 #include <vector>
 #include <map>
 #include <list>
 #include <set>
-#if defined(_MSC_VER) && _MSC_VER<=1200
-#   pragma warning(pop)
-#endif
 
 #if defined(_UNIX)
 #   define ANT_UNIX
@@ -49,31 +35,12 @@
 #   include <GL/glx.h>
 #   include <X11/Xatom.h>
 #   include <unistd.h>
-#   undef _WIN32
-#   undef WIN32
-#   undef _WIN64
-#   undef WIN64
-#   undef _WINDOWS
-#   undef ANT_WINDOWS
-#   undef ANT_OSX
 #elif defined(_MACOSX)
 #   define ANT_OSX
 #   include <unistd.h>
 #   include <Foundation/Foundation.h>
 #   include <NSImage.h>
 #   include <NSCursor.h>
-#   undef _WIN32
-#   undef WIN32
-#   undef _WIN64
-#   undef WIN64
-#   undef _WINDOWS
-#   undef ANT_WINDOWS
-#   undef ANT_UNIX
-#elif defined(_WINDOWS) || defined(WIN32) || defined(WIN64) || defined(_WIN32) || defined(_WIN64)
-#   define ANT_WINDOWS
-#   define WIN32_LEAN_AND_MEAN // Exclude rarely-used stuff from Windows headers
-#   include <windows.h>
-#   include <shellapi.h>
 #endif
 
 #if defined(ANT_OSX)
